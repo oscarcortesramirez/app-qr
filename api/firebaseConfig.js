1b020060ab0fb280f4b761e889932d558a340f66
OSCAR MAURICIO CORTES RAMIREZ <2020371018@uteq.edu.mx>
2023-11-15 23:11:44 -0600

    added login firebase

---
+++ api/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBVHHb761nJc7h8Kr9DDegI4grCM33yp8A",
+    authDomain: "app-qr-7c881.firebaseapp.com",
+    projectId: "app-qr-7c881",
+    storageBucket: "app-qr-7c881.appspot.com",
+    messagingSenderId: "94415786384",
+    appId: "1:94415786384:web:61aba2e340ba351a91a315",
+    measurementId: "G-XVPG7YFQ16"
+  };
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- app/index.js
+++ app/index.js
@@ -1,12 +1,40 @@
-import { View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
-
+import { View, StyleSheet, Button, TextInput, inp } from "react-native";
+import { router } from "expo-router";
+import { useState } from "react";
+import { app } from '../api/firebaseConfig';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 export default function Page() {
+
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+
+    const auth = getAuth(app)
+
+    const isNumeric = (value) => /^\d+$/.test(value.split('@')[0]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    
+        signInWithEmailAndPassword(auth, user, password)
+            .then((userCredential) => {
+                alert('Bienvenido')
+                if (isNumeric(user)) {
+                    router.replace('/QRGenerate') // Profesor
+                } else {
+                    router.replace('/Escanear'); // Alumno
+                }
+            })
+            .catch((error) => {
+                alert(error.message)
+            })
+    }
+
     return (
         <View style={styles.container} >
-            <Link href={"/QRGenerate"} >Inicio de Sesión Profesor</Link>
-            <Link href={"/Escanear"} >Inicio de Sesión Alumno</Link>
+            <TextInput value={user} placeholder={'Correo electrónico'} onChangeText={setUser} keyboardType="email-address" />
+            <TextInput value={password} placeholder={'Contraseña'} onChangeText={setPassword} secureTextEntry={true} />
+            <Button type="button" onPress={handleSubmit} title={'Entrar'} />
         </View>
     )
 }

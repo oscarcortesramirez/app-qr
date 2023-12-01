5cb605789bf31ae96aed180d14e9457a127ef9e3
OSCAR MAURICIO CORTES RAMIREZ <2020371018@uteq.edu.mx>
2023-11-30 19:49:57 -0600

    added student info

--- app/QRGenerate.js
+++ app/QRGenerate.js
@@ -2,15 +2,45 @@ import { StyleSheet, Text, View, TextInput } from 'react-native';
 import PanelQRCode from '../components/PanelQRCode';
 import QRCode from '../components/QRCode';
 import { QRCodeProvider } from '../providers/QRCodeProvider';
+import { getAuth } from 'firebase/auth';
+import app from '../api/firebaseConfig';
+import { doc, getDoc, getFirestore } from 'firebase/firestore'
+import { useEffect, useState } from 'react';
 
 export default function QRGenerate() {
-  
+
+  const [user , setUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth(app);
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const uid = user.uid;
+        const email = user.email;
+        const db = getFirestore(app);
+        const docRef = doc(db, 'users', uid);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          const userData = {...docSnap.data(), email};
+          setUser(userData);
+        } else {
+          console.log('Ocurrio un error');
+        }
+      } else {
+        console.log('Usuario no existe')
+      }
+    });
+
+    return () => unsubscribe;
+  }, [])
+
   return (
     <View style={styles.container}>
       
       <QRCodeProvider>
         {/* <PanelQRCode /> */}
-        <QRCode />
+        <QRCode usuario={user} />
       </QRCodeProvider>
     </View>
   );
--- components/QRCode.js
+++ components/QRCode.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
 import SvgQRCode from "react-native-qrcode-svg";
 import { useContextPanelQRCode } from "../providers/QRCodeProvider";
 import { router } from "expo-router";
+import { getAuth, signOut } from "firebase/auth";
+import app from "../api/firebaseConfig";
 
-export default function QRCode() {
+export default function QRCode({ usuario }) {
   const [state] = useContextPanelQRCode();
 
- 
+  const [nombre, setNombre] = useState();
+  const [grupo, setGrupo] = useState();
+  const [correo, setCorreo] = useState();
 
-  const handleLogout = () => {
-    router.replace('/')
+  useEffect(() => {
+    if (usuario) {
+      setNombre(usuario.nombre);
+      setGrupo(usuario.grupo);
+      setCorreo(usuario.email);
+    }
+  }, [usuario])
+
+  const handleLogout = async () => {
+    try {
+      const auth = getAuth();
+      await signOut(auth);
+      router.replace('/')
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>{`¡Bienvenido ${nombre || ''} !`}</Text>
+      <Text style={styles.title}>{`Grupo ${grupo || ''}`}</Text>
+      <Text style={styles.title}>{`Correo: ${correo || ''} `}</Text>
       <Text style={styles.title}>Escanear Código QR</Text>
       <View style={styles.qrCodeContainer}>
-        <SvgQRCode value={state.text} size={state.size} color={state.color} />
+        <SvgQRCode value={nombre || 'texto'} size={state.size} color={state.color} />
       </View>
       <TouchableOpacity
         style={styles.logoutButtonContainer}
